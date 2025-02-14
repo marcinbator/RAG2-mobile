209699d698e28f5a32c536c02b3314613572f6dd
Marcin Bator <marcinbator.ofc@gmail.com>
2025-02-14 12:01:22 +0100

    feat: #main games as list tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:rag_2_mobile/models/Game.dart';
 import 'package:rag_2_mobile/screens/game_selection_page.dart';
 import 'package:rag_2_mobile/screens/game_page.dart';
 
@@ -19,13 +20,13 @@ void main() {
 
   group('Home Page Tests', () {
     testWidgets('Should display IP address and connected devices count', (tester) async {
-      await tester.pumpWidget(const MaterialApp(home: GamePage(title: 'Pong', gamePath: '/pong')));
+      await tester.pumpWidget(const MaterialApp(home: GamePage(game: Game('Pong', '/pong'))));
       await tester.pumpAndSettle();
       expect(find.text('Połączonych urządzeń: 0'), findsOneWidget);
     });
 
     testWidgets('Should send correct action when control button is tapped', (tester) async {
-      await tester.pumpWidget(const MaterialApp(home: GamePage(title: 'Pong', gamePath: '/pong')));
+      await tester.pumpWidget(const MaterialApp(home: GamePage(game: Game('Pong', '/pong'))));
 
       expect(find.text("Góra"), findsOneWidget);
 
